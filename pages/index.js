3645ec9db4ceca850cc468fa6ae32095e65a9ed3
felipecr-wlp <felipe.cr@pavific.com>
2026-08-07 17:23:27 -0600

    fix: server-side redirect, vercel.json con framework y output explicitos

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,9 @@
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
+export async function getServerSideProps({ res }) {
+  res.writeHead(302, { Location: '/embed' })
+  res.end()
+  return { props: {} }
+}
 
 export default function Home() {
-  const { replace, asPath } = useRouter()
-  useEffect(() => {
-    const search = asPath.includes('?') ? asPath.substring(asPath.indexOf('?')) : ''
-    replace('/embed' + search)
-  }, [])
-  return (
-    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh', fontFamily: 'system-ui', color: '#94a3b8', fontSize: 14 }}>
-      Cargando...
-    </div>
-  )
+  return null
 }
